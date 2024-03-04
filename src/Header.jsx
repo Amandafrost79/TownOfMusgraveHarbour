031e7284b4be7cf4832937a6ebb208a413685b14
Amanda Frost <amandafrost79@outlook.com>
2024-03-03 22:17:03 -0330

    adjust toggle icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -41,15 +41,19 @@
 // }
 
 // export default Header;
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import logo from "/images/Musgrave.png";
 import "./Header.css";
 
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar bg="light" expand="lg" sticky="top" expanded={expanded}>
       <Container>
         <Navbar.Brand as={Link} to="/TownOfMusgraveHarbour">
           <img
@@ -60,7 +64,13 @@ const Header = () => {
             alt="Municipality Logo"
           />{" "}
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+        >
+          <FontAwesomeIcon icon={expanded ? faTimes : faBars} />
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/TownOfMusgraveHarbour">
